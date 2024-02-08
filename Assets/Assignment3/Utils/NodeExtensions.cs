b5b38aa04ee8c46129ec189fc5a351bbd12ee954
leggy <lupa984@gmail.com>
2024-02-08 12:39:23 +0100

    created export logic

---
+++ Assets/Assignment3/Utils/NodeExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Assignment3.Editor;
+using Assignment3.Editor.Nodes;
+
+namespace Assignment3.Utils
+{
+    public static class NodeExtensions
+    {
+        public static NodeFramework.SerializableNode ToSerializableNode(this Node node)
+        {
+            return node switch
+            {
+                SingleNode sn => new NodeFramework.SerializableSingleNode
+                {
+                    startNode = sn.IsStart, 
+                    name = sn.name, 
+                    id = sn.id.ToString(), 
+                    title = sn.title,
+                    type = sn.GetType().ToString()
+                },
+                BinaryNode bn => new NodeFramework.SerializableBinaryNode
+                {
+                    startNode = bn.IsStart,
+                    title = bn.title,
+                    id = bn.id.ToString(),
+                    type = bn.GetType().ToString()
+                },
+                _ => throw new ArgumentOutOfRangeException(nameof(node))
+            };
+        }
+    }
+}
